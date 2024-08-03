d639e9f8dc507367264adf884d31fe3cc1462171
wesleyara <wes1gom@gmail.com>
2024-08-03 14:45:30 -0300

    chore: starting project structure with, tailwind, eslint and prettier

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to the Search Band App</h1>
+      <Image src="/next.svg" alt="Next.js logo" width={200} height={200} />
+    </main>
+  );
+}
