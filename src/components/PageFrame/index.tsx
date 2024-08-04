9a5fa2217393ea4598df7c49644d69df7110fb58
wesleyara <wes1gom@gmail.com>
2024-08-04 15:35:23 -0300

    feat: Refactor PageFrame component and improve search functionality

--- src/components/PageFrame/index.tsx
+++ src/components/PageFrame/index.tsx
@@ -2,7 +2,6 @@
 
 import { useData } from "~/context/DataContext";
 import { FaSearch } from "react-icons/fa";
-import { MdClear } from "react-icons/md";
 
 import { Search } from "../Search";
 import { SelectAttraction } from "../SelectAttraction";
@@ -14,16 +13,31 @@ export const PageFrame = () => {
     ticketSearchIsLoading,
     handleSearch,
     handleChange,
-    handleClear,
   } = useData();
 
+  const enableToRender = ticketSearchData && ticketSearchData.length > 0;
+
   return (
-    <main className="h-[100vh] flex flex-col items-center justify-center bg-[#ebebeb]">
+    <main className="flex flex-col items-center my-24">
       <Search.Root>
         <Search.Input onChange={handleChange} value={searchQuery} />
         <Search.Button icon={FaSearch} onClick={handleSearch} />
-        {searchQuery && <Search.Button icon={MdClear} onClick={handleClear} />}
       </Search.Root>
+
+      {ticketSearchIsLoading ? (
+        <p className="mt-10">Carregando...</p>
+      ) : (
+        enableToRender && (
+          <div className="flex flex-col gap-3 mt-5 overflow-auto md:w-[600px] w-[90%]">
+            {ticketSearchData.map(item => (
+              <SelectAttraction.Root key={item.id}>
+                <SelectAttraction.Content item={item} />
+                <SelectAttraction.Button />
+              </SelectAttraction.Root>
+            ))}
+          </div>
+        )
+      )}
     </main>
   );
 };
