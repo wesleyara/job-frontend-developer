5e6af03ae1a69c20d98f62783a22bc6c5d7593f0
wesleyara <wes1gom@gmail.com>
2024-08-06 17:49:58 -0300

    feat: update DataProvider to handle YouTube video queries

--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useToast } from "~/components/ui/use-toast";
 import { HttpClient } from "~/lib/httpClient";
-import { SearchAttraction } from "~/types";
+import { SearchAttraction, YoutubeDetailsType } from "~/types";
 import {
   ChangeEvent,
   createContext,
@@ -15,10 +15,15 @@ import {
 
 interface DataContextType {
   searchQuery: string;
-  ticketSearchData: SearchAttraction[];
+  dataTicketSearch: SearchAttraction[];
   selectedAttraction: SearchAttraction | undefined;
-  ticketSearchIsLoading: boolean;
-  ticketSearchError: Error | null;
+  isLoadingTicketSearch: boolean;
+  errorTicketSearch: Error | null;
+  dataYoutubeVideosDetails: YoutubeDetailsType[] | undefined;
+  isLoadingYoutubeVideos: boolean;
+  isLoadingYoutubeVideosDetails: boolean;
+  errorYoutubeVideos: Error | null;
+  errorYoutubeVideosDetails: Error | null;
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
   handleSearch: (e: FormEvent) => void;
   handleSelectAttraction: (item: SearchAttraction) => void;
@@ -31,6 +36,7 @@ interface DataProviderProps {
 export const DataContext = createContext({} as DataContextType);
 
 export const DataProvider = ({ children }: DataProviderProps) => {
+  const queryClient = useQueryClient();
   const httpClient = new HttpClient();
 
   const { toast } = useToast();
@@ -38,12 +44,12 @@ export const DataProvider = ({ children }: DataProviderProps) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedAttraction, setSelectedAttraction] =
     useState<SearchAttraction>();
-  const [enabled, setEnabled] = useState(false);
 
   const {
-    data: ticketSearchData,
-    isLoading: ticketSearchIsLoading,
-    error: ticketSearchError,
+    data: dataTicketSearch,
+    isLoading: isLoadingTicketSearch,
+    error: errorTicketSearch,
+    refetch: refetchTicketSearch,
   } = useQuery({
     queryKey: ["ticket_search"],
     queryFn: async () => {
@@ -64,7 +70,65 @@ export const DataProvider = ({ children }: DataProviderProps) => {
 
       return data._embedded.attractions;
     },
-    enabled,
+    enabled: false,
+    retry: false,
+  });
+
+  const {
+    data: dataYoutubeVideos,
+    isLoading: isLoadingYoutubeVideos,
+    error: errorYoutubeVideos,
+  } = useQuery({
+    queryKey: ["youtube_videos"],
+    queryFn: async () => {
+      if (!selectedAttraction) {
+        throw new Error("Selecione uma atração para buscar vídeos");
+      }
+
+      const data = await httpClient.get(
+        "youtube",
+        `/search?part=snippet&maxResults=5&q=${selectedAttraction.name}&type=video`,
+      );
+
+      if (!data?.items) {
+        throw new Error(
+          `Não existe nenhum dado para ser exibido para ${selectedAttraction.name}`,
+        );
+      }
+
+      const videoIds = data.items.map((item: any) => item.id.videoId).join(",");
+
+      return videoIds;
+    },
+    enabled: !!selectedAttraction,
+    retry: false,
+  });
+
+  const {
+    data: dataYoutubeVideosDetails,
+    isLoading: isLoadingYoutubeVideosDetails,
+    error: errorYoutubeVideosDetails,
+  } = useQuery<YoutubeDetailsType[]>({
+    queryKey: ["youtube_videos_details"],
+    queryFn: async () => {
+      if (!selectedAttraction) {
+        throw new Error("Selecione uma atração para buscar vídeos");
+      }
+
+      const data = await httpClient.get(
+        "youtube",
+        `/videos?id=${dataYoutubeVideos}&part=snippet,contentDetails,statistics,status`,
+      );
+
+      if (!data?.items) {
+        throw new Error(
+          `Não existe nenhum dado para ser exibido para ${selectedAttraction.name}`,
+        );
+      }
+
+      return data.items;
+    },
+    enabled: !!dataYoutubeVideos,
     retry: false,
   });
 
@@ -77,13 +141,24 @@ export const DataProvider = ({ children }: DataProviderProps) => {
       });
     }
 
-    setEnabled(true);
     setSelectedAttraction(undefined);
+    queryClient.removeQueries({
+      queryKey: ["ticket_search"],
+      exact: true,
+    });
+    queryClient.removeQueries({
+      queryKey: ["youtube_videos"],
+      exact: true,
+    });
+    queryClient.removeQueries({
+      queryKey: ["youtube_videos_details"],
+      exact: true,
+    });
+    refetchTicketSearch();
   };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setSearchQuery(e.target.value);
-    setEnabled(false);
   };
 
   const handleSelectAttraction = (item: SearchAttraction) => {
@@ -95,10 +170,15 @@ export const DataProvider = ({ children }: DataProviderProps) => {
     <DataContext.Provider
       value={{
         searchQuery,
-        ticketSearchData,
+        dataTicketSearch,
         selectedAttraction,
-        ticketSearchIsLoading,
-        ticketSearchError,
+        isLoadingTicketSearch,
+        errorTicketSearch,
+        dataYoutubeVideosDetails,
+        isLoadingYoutubeVideos,
+        isLoadingYoutubeVideosDetails,
+        errorYoutubeVideos,
+        errorYoutubeVideosDetails,
         handleChange,
         handleSearch,
         handleSelectAttraction,
